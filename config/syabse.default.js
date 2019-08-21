19b2b27f48a5033323f20981936803c0363fbc85
AzizSudrajat <azizsudrajat95@gmail.com>
2019-08-21 14:12:03 +0700

    Add All DATA

---
+++ config/syabse.default.js
@@ -0,0 +1,27 @@
+/*
+ * Use this file to configure you mysql data connector
+ *
+ * By default, MYSQL username and password are environment variables.
+ * username MYSQL_USER, password MYSQL_PASSWORD
+ * Example setting of environment variables:
+ * linux/mac: export MYSQL_PASSWORD=password
+ * windows: setx MYSQL_PASSWORD 'password'
+ */
+module.exports = {
+	connectors: {
+		sybase: {
+			connector: 'api-builder-plugin-dc-sybase',
+			host: 'Emerio-AzisSudrajat',
+			port: 5000,
+			database: 'kampus',
+			user: 'sa',
+			password: 'P@ssw0rd',
+
+			// Create models based on your schema that can be used in your API.
+			// generateModelsFromSchema: true,
+
+			// Whether or not to generate APIs based on the methods in generated models.
+			// modelAutogen: false
+		}
+	}
+};
