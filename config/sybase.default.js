3e2f96419d02d0cc94055a0c217ad52255aafc71
Ridwan Effendi <ridwan.effendi.g@gmail.com>
2019-08-22 11:59:35 +0700

    tes

--- config/sybase.default.js
+++ config/sybase.default.js
@@ -16,6 +16,7 @@ module.exports = {
 			database: 'kampus',
 			user: 'sa',
 			password: 'P@ssw0rd'
+			
 
 			// Create models based on your schema that can be used in your API.
 			// generateModelsFromSchema: true,
